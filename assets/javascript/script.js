987d81373a78bf8946e1fe99bef116ecf82616bf
Origin 42 <daveplum1991@gmail.com>
2022-03-17 12:57:34 +0800

    Updated localStorage function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -38,30 +38,54 @@ const getDay = () => {
     return daySelected;
 }
 
+// Check the time
 let currentTime = "";
-// Render Time
+// Set data to load on call
 const renderTime = () => {
     $("#Timeslots").empty();
 
+    // Render Time on Page Load
+    let currentDaySellected = getDay();
+
     hoursAvailable().forEach(element => {
-        let tr = $(`<tr id="${trim(element)}_tr">`);
-        let tdRow = $(`<td scope="row">`);
-        let tdCol = $(`<td id="${trim(element)}_input" colspan="8">`);
-        let button = $(`<button id="${trim(element)}_button" type="button" class="btn btn-primary .saveBtn"><i class="fa-solid fa-floppy-disk fa-x2"></i></button>`);
+        // Create a unique id to prepend to each input and button element
+        let ids = `${currentDaySellected}${element.replace(/ /g, '')}`
+        
+        // Create variables to render
+        let tr = $(`<tr id="${ids}_tr">`);
+        let tdRow = $(`<td scope="row">`).text(element);
+        let tdCol = $(`<td colspan="8">`).append(`<input id="${ids}_input" class="eventInput" type="text" placeholder="Derps">`);
+        let button = $(`<button id="${ids}_button" type="button" class="btn btn-primary .saveBtn"><i class="fa-solid fa-floppy-disk fa-x2"></i></button>`);
 
-        let inputfield = $(`${trim(element)}_input`);
-        $(`#${trim(element)}_button`).on('click', function () {
-            let dataInput = $(inputfield);
+        // Add tags to page
+        tr.append(tdRow);
+        tr.append(tdCol);
+        tr.append(button);
+        $("#Timeslots").append(tr);
 
-            // Save input
 
+        // Create an even listener for each button
+        $(`#${ids}_button`).on('click', function () {
+            // Capture input value
+            let dataInput = $(`#${ids}_input`);
+            // Save input value to localStorage
+            localStorage.setItem(JSON.stringify(`${ids}_storedEvent`), dataInput.val());
         })
 
+        
+        
+
+        // Add data to page on load and/or click
+        // tdRow.text(element)
+        // if (localStorage.getItem(JSON.parse(`${element}_event`)).value) {
+        //     tdCol.text(localStorage.getItem(JSON.parse(`${element}_event`)).value);
+        // }
+
         // Add if to check if time is less than current time (hour), the same as or greater
-        let currentHour = new Date().getHours();
-        let 
-        if (times)
-        $("#Timeslots").append(tr.text(element))
+        // let currentHour = new Date().getHours();
+        // let 
+        // if (times)
+        // $("#Timeslots").append(tr.text(element))
     });
 }
 
@@ -72,8 +96,12 @@ const renderTime = () => {
 
 
 // Event listeners 
-calenderDateInput.on('change', getDay);
+calenderDateInput.on('change', renderTime);
 
 // Function calls
 setInterval(setTime, 1000);
 // change colors every time the hour passes.
+
+
+
+
