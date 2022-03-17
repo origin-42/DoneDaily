16fe41800c210e9a695616aafb0c48be31e6dc81
Origin 42 <daveplum1991@gmail.com>
2022-03-18 00:05:17 +0800

    implementing color change

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -39,7 +39,7 @@ const getDay = () => {
 }
 
 // Check the time
-let currentTime = "";
+let currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');;
 // Set data to load on call
 const renderTime = () => {
     $("#Timeslots").empty();
@@ -53,9 +53,9 @@ const renderTime = () => {
         
         // Create variables to render
         let tr = $(`<tr id="${ids}_tr">`);
-        let tdRow = $(`<td scope="row">`).text(element);
+        let tdRow = $(`<td id="${ids}_time" class="tableTime" scope="row">`).text(element);
         // Add stored Event for Input
-        let inputItem = $(`<input id="${ids}_input" class="eventInput" type="text" placeholder="Derps">`);
+        let inputItem = $(`<input id="${ids}_input" class="eventInput" type="text">`);
         inputItem.val(localStorage.getItem(`${ids}_storedEvent`));
         let tdCol = $(`<td colspan="8">`).append(inputItem);
         let button = $(`<button id="${ids}_button" type="button" class="btn btn-primary .saveBtn"><i class="fa-solid fa-floppy-disk fa-x2"></i></button>`);
@@ -76,6 +76,51 @@ const renderTime = () => {
         })
 
     });
+
+    // Color code hours
+    colorCode(currentTime);
+}
+
+// Change colors depending on current time.
+const colorCode = (day) => {
+    // Grab formatting
+    let hour = `${moment(day, "MMMM Do YYYY, h:mm:ss a").format(`h A`)}`;
+
+    // Grab class array of times rendered
+    let tableTimeHours = $(`.tableTime`).map(function () {
+        return $(this).attr('id');
+    })
+    for (let i = 0; i < tableTimeHours.length; i++) {
+
+        // console.log(`${moment(day, "MMMM Do YYYY, h:mm:ss a").format(`HH`)}`)
+        // console.log(`${moment(day, "MMMM Do YYYY, h:mm:ss a").format(`D`)}`)
+        // console.log(`${moment(day, "MMMM Do YYYY, h:mm:ss a").format(`M`)}`)
+        // console.log(`${moment(day, "MMMM Do YYYY, h:mm:ss a").format(`Y`)}`)
+        
+        // Change each index to suit day format
+        let trim = tableTimeHours[i].replace("_time", "");
+        console.log(moment(trim, 'MMDDYYYYhA').format('HH'))
+        // console.log(moment(trim, 'MMDDYYYYhA').format('D'))
+        // console.log(moment(trim, 'MMDDYYYYhA').format('M'))
+        // console.log(moment(trim, 'MMDDYYYYhA').format('Y'))
+        let hoursFormat = moment(trim, 'MMDDYYYYhA').format(`h A`);
+        if (hoursFormat === hour) {
+            // Change style for background
+        } 
+        // Format for testing
+        let currentHour = `${moment(day, "MMMM Do YYYY, h:mm:ss a").format(`HH`)}`;
+        let currentDay = `${moment(day, "MMMM Do YYYY, h:mm:ss a").format(`D`)}`;
+        let month = `${moment(day, "MMMM Do YYYY, h:mm:ss a").format(`M`)}`;
+        let year = `${moment(day, "MMMM Do YYYY, h:mm:ss a").format(`Y`)}`;
+        
+        let selectedHour = moment(trim, 'MMDDYYYYhA').format('HH');
+        let selectedDay = moment(trim, 'MMDDYYYYhA').format('D');
+        let selectedMonth = moment(trim, 'MMDDYYYYhA').format('M');
+        let selectedYear = moment(trim, 'MMDDYYYYhA').format('Y');
+
+        
+        
+    }
 }
 
 // Event listeners 
